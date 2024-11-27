64d6c0e60e93ff239ac098cdb71f1697630682f6
Sergei/Nikiforov <170589546+SerNikif@users.noreply.github.com>
2024-11-27 16:28:52 +0800

    Add files via upload

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from sqlalchemy import Column, Integer, String
 
 engine = create_engine('sqlite:///taskmanager', echo=True)
 
